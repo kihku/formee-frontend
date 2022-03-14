bee617b69140be7d150ca76de51170294f75f1bf
ththanh18 <55954524+ththanh18@users.noreply.github.com>
2022-03-14 23:08:42 +0700

    add prettier  husky lint-staged

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    arrowParens: "avoid",
+    bracketSpacing: true,
+    printWidth: 120,
+    semi: true,
+    singleQuote: false,
+    tabWidth: 2,
+    trailingComma: "all",
+    endOfLine: "auto",
+};
