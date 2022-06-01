74b39901367771e34ae8745f178934a06f343773
Phạm Trần Hiền Dung <pthdung2903@gmail.com>
2022-06-01 23:53:14 +0700

    fix url profile

--- src/apis/axiosClient.tsx
+++ src/apis/axiosClient.tsx
@@ -3,11 +3,11 @@ import { getCookie } from "utils/cookieUtils";
 
 export const URL_PROFILE = {
   DEV: "http://localhost:8080", // local
-  PRO: "http://103.101.162.22:8080", // server
+  PRO: "https://api.formee.website", // server
 };
 
 const AXIOS_INSTANCE = axios.create({
-  baseURL: URL_PROFILE.DEV,
+  baseURL: URL_PROFILE.PRO,
   timeout: 10000 * 120,
   headers: {
     "Content-Type": "application/json",
