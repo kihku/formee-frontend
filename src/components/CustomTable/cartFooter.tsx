8f9edbf315487f1e536fdf5ff83ccab250f53cc3
Trần Hoàng Thanh <ththanh5200@gmail.com>
2022-06-01 23:03:52 +0700

    cart footer

--- src/components/CustomTable/cartFooter.tsx
+++ src/components/CustomTable/cartFooter.tsx
@@ -1,25 +1,67 @@
-import { Box, Grid } from "@mui/material";
-import React from "react";
+import { Box, Grid, InputAdornment, Typography } from "@mui/material";
+import { StyledInput } from "components/CustomTextField";
+import React, { useEffect, useState } from "react";
 import { COLORS } from "styles";
 
-export interface CustomCartFooterProps {}
+export interface CustomCartFooterProps {
+  formik: any;
+  index: number;
+}
 
-const CustomCartFooter: React.FC<CustomCartFooterProps> = () => {
+const CustomCartFooter: React.FC<CustomCartFooterProps> = ({ formik, index }: CustomCartFooterProps) => {
+  const [total, setTotal] = useState<number>(0);
+  const [subTotal, setSubTotal] = useState<number>(0);
+  const [discount, setDiscount] = useState<number>(0);
+
+  const updateTotal = () => {
+    let newTotal: number = 0;
+    let discountPercentage: number = Math.round((100 - discount) / 100);
+    formik.values["response"].at(index).forEach((product: any) => {
+      newTotal += product.productPrice * product.quantity;
+    });
+    setTotal(newTotal * discountPercentage);
+    setSubTotal(newTotal);
+  };
+
+  useEffect(() => {
+    updateTotal();
+  }, [formik.values, discount]);
+
   return (
-    <Grid xs={12} sx={{ display: "flex", flexDirection: "column", textAlign: "end", gap: 2, paddingTop: 3 }}>
+    <Grid item xs={12} sx={{ display: "flex", flexDirection: "column", textAlign: "end", gap: 2, paddingTop: 3 }}>
       <Box sx={{ display: "flex", justifyContent: "space-between", fontWeight: 500 }}>
         <Box>Subtotal</Box>
-        <Box>0đ</Box>
+        <Box>{subTotal}đ</Box>
       </Box>
-      <Box sx={{ display: "flex", justifyContent: "space-between", fontWeight: 500 }}>
+      <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", fontWeight: 500 }}>
         <Box>Discount</Box>
-        <Box>0đ</Box>
+        <StyledInput
+          type="number"
+          sx={{
+            maxWidth: "7vw",
+            "& .MuiInputBase-input": {
+              textAlign: "center",
+            },
+          }}
+          inputProps={{
+            min: 0,
+            max: 100,
+          }}
+          endAdornment={
+            <InputAdornment position="end">
+              <Typography sx={{ fontWeight: 500 }}>%</Typography>
+            </InputAdornment>
+          }
+          onChange={e => {
+            setDiscount(Number(e.target.value));
+          }}
+        />
       </Box>
       <Box
         sx={{ display: "flex", justifyContent: "space-between", fontWeight: 600, fontSize: 18, color: COLORS.primary }}
       >
         <Box>Total</Box>
-        <Box>0đ</Box>
+        <Box>{total - discount}đ</Box>
       </Box>
     </Grid>
   );
